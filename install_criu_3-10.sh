b12c82d4abc4b04c955e64d1f31e8b03223462a3
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2020-12-02 15:25:10 +0100

    Install script for CRIU 3.10 added

---
+++ install_criu_3-10.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+apt update
+apt upgrade
+apt install build-essential \
+			pkg-config \
+			libnet-dev python-yaml libaio-dev \
+			libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev python-future
+
+# criu install
+curl -O -sSL http://download.openvz.org/criu/criu-3.10.tar.bz2
+tar xjf criu-3.10.tar.bz2 
+cd criu-3.10
+make
+cp ./criu/criu /usr/local/bin
+cd
