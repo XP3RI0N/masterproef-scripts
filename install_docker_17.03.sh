ebd844e5388f9eb4a1cf29ae488cb66aa0f0cec0
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2020-12-02 16:14:03 +0100

    Install script for Docker 17.03 added

---
+++ install_docker_17.03.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+experimental=false
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    --experimental)
+      experimental=true
+      ;;
+    *)
+      echo "Argument: $1 does not exist."
+      ;;
+  esac
+  shift
+done
+
+apt-get update
+
+# Install docker requirements
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+# Install docker
+apt-get update
+apt-get install -y docker-ce=17.03.0~ce-0~ubuntu-xenial containerd.io
+
+if [ $experimental ]
+then
+    # Setup daemon.
+    cat > /etc/docker/daemon.json <<EOF
+    {
+    "experimental": true
+    }
+EOF
+
+    # Restart docker.
+    systemctl daemon-reload
+    systemctl restart docker
+fi
