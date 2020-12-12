b4ad18bef3f8a24bc3384e69e1e586ca28b15f3a
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2020-12-12 16:46:33 +0100

    docker clone script moddified

--- clone/clone_docker.sh
+++ clone/clone_docker.sh
@@ -2,12 +2,11 @@
 set -e
 
 # Run container to migrate
-# docker run -d --name looper2 --security-opt seccomp:unconfined busybox \
-#         /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
+# docker run -d --name looper --security-opt seccomp:unconfined busybox /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
 
-docker checkpoint create --checkpoint-dir=/tmp looper checkpoint2
+docker checkpoint create --checkpoint-dir=/tmp looper checkpoint1
 
 docker create --name looper-clone --security-opt seccomp:unconfined busybox \
 	/bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
 
-$ docker start --checkpoint-dir=/tmp --checkpoint=checkpoint2 looper-clone
+$ docker start --checkpoint-dir=/tmp --checkpoint=checkpoint1 looper-clone
