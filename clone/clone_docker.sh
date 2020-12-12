a5f46b092ce7403fb4e48a9c5ae46eeddad942be
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2020-12-12 16:48:59 +0100

    typo corrected

--- clone/clone_docker.sh
+++ clone/clone_docker.sh
@@ -9,4 +9,4 @@ docker checkpoint create --checkpoint-dir=/tmp looper checkpoint1
 docker create --name looper-clone --security-opt seccomp:unconfined busybox \
 	/bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
 
-$ docker start --checkpoint-dir=/tmp --checkpoint=checkpoint1 looper-clone
+docker start --checkpoint-dir=/tmp --checkpoint=checkpoint1 looper-clone
