4c920cfd32f0d136b25565992e5ae9d28172e1b5
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2021-07-31 18:52:32 +0200

    close std

--- python/large_container_toFIle/app.py
+++ python/large_container_toFIle/app.py
@@ -1,4 +1,17 @@
 import time
+import sys
+import os
+
+# here's the obvious part!
+sys.stdin.close()
+sys.stdout.close()
+sys.stderr.close()
+
+# this is pretty obscure!
+os.close(0)
+os.close(1)
+os.close(2)
+
 
 large_string = " " * (1024 ** 3)
 
